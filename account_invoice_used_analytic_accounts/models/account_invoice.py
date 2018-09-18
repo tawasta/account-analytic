e2ebb84754fce6c654a8a7a4dc0e9f08d6be2dde
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-09-18 12:54:10 +0300

    New module account_invoice_used_analytic_accounts

---
+++ account_invoice_used_analytic_accounts/models/account_invoice.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = 'account.invoice'
+
+    @api.depends('invoice_line_ids')
+    def _compute_row_analytic_account_ids(self):
+        for invoice in self:
+            invoice.row_analytic_account_ids \
+                = invoice.invoice_line_ids.mapped('account_analytic_id')
+
+    row_analytic_account_ids = fields.Many2many(
+        compute='_compute_row_analytic_account_ids',
+        comodel_name='account.analytic.account',
+        relation='invoice_row_analytic_rel',
+        column1='invoice_id',
+        column2='analytic_account_id',
+        string="Lines' analytic accounts",
+        store=True,
+        help='List of analytic accounts used in the invoice rows'
+    )
