015aa95a48a643678c5f632ac0d1830dde0c4ce3
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-09-25 15:25:30 +0300

    Fix bug with archived tags not showing in search results

--- account_invoice_used_analytic_tags/__manifest__.py
+++ account_invoice_used_analytic_tags/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': "Lines' analytic tags for invoices",
     'summary': "Enables filtering invoices based on lines' analytic tags",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Invoicing',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
@@ -35,6 +35,7 @@
     },
     'depends': [
         'account',
+        'account_analytic_tag_disable',
     ],
     'data': [
     ],
