90508459335007a77226532a4f0a1f7cb47f20ec
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-03-31 18:23:43 +0300

    [IMP] Disable the field on non-drafts

--- account_vendor_invoice_update_line_analytic_accounts/models/account_invoice.py
+++ account_vendor_invoice_update_line_analytic_accounts/models/account_invoice.py
@@ -9,6 +9,7 @@ class AccountInvoice(models.Model):
     analytic_account_id = fields.Many2one(
         comodel_name='account.analytic.account',
         string='Default Analytic Account',
+        readonly=True,
         states={'draft': [('readonly', False)]},
         help=_('Informational analytic account related to the invoice')
     )
