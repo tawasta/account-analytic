9c8dbe13e47a9cff15301f30689244f2ddfd9fbb
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-03-05 12:55:35 +0200

    [IMP] Allow normal users to upload XML files

--- account_bank_statement_import_camt_multi/__manifest__.py
+++ account_bank_statement_import_camt_multi/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "CAMT Format Bank Statements Import multiple statements",
     "summary": "Import multiple CAMT statements from a same file",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.2.0",
     "category": "Invoicing",
     "website": "https://github.com/Tawasta/account-analytic",
     "author": "Tawasta",
