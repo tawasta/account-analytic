8318e7a4a4c5d22ea3e629798020a4ae7c557b0a
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-03-05 09:35:34 +0200

    [FIX] Actually working multi-importer and a correct return

--- account_bank_statement_import_camt_multi/wizard/account_bank_statement_import.py
+++ account_bank_statement_import_camt_multi/wizard/account_bank_statement_import.py
@@ -1,16 +1,22 @@
+import logging
+import base64
+
 from lxml import etree
 from odoo import api
 from odoo import models
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountBankStatementImport(models.TransientModel):
     _inherit = "account.bank.statement.import"
 
-    @api.model
-    def _parse_file(self, data_file):
-        data_str = str(data_file, "utf-8")
+    @api.multi
+    def import_files(self):
+        data_str = str(base64.b64decode(self.data_file), "utf-8")
         statements = data_str.split('<?xml version="1.0" encoding="UTF-8"?>')
         parser = etree.XMLParser(recover=True, remove_comments=True)
+
         for statement in statements:
             if not statement:
                 # The split will result for the first item to be empty
@@ -25,15 +31,18 @@ class AccountBankStatementImport(models.TransientModel):
             if root is None:
                 raise ValueError("Not a valid xml file, or not an xml file at all.")
 
-            res = super(AccountBankStatementImport, self)._parse_file(
-                etree.tostring(root)
-            )
+            _logger.debug("Try parsing with camt.")
+            statement_str = etree.tostring(root)
 
-            if statement == statements[-1]:
-                # Return res for the last item
-                return res
+            self.data_file = base64.b64encode(statement_str)
+            res = self.import_file()
+
+        action = self.env.ref("account.action_bank_statement_tree").read()[0]
+
+        return action
 
     def _find_additional_data(self, currency_code, account_number):
+        # Auto-match the journal
         context = {}
         bank = self.env["res.partner.bank"].search(
             [("sanitized_acc_number", "=", account_number)]
