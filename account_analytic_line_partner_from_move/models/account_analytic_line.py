aa071d71521ede3db0907c49bdc3b9ec67b42168
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-09-29 06:22:02 +0000

    [IMP] account_vendor_invoice_update_line_analytic_tags module translations

--- account_analytic_line_partner_from_move/models/account_analytic_line.py
+++ account_analytic_line_partner_from_move/models/account_analytic_line.py
@@ -1,16 +1,17 @@
-
 from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
 
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     account_move_partner_id = fields.Many2one(
-        related='move_id.partner_id',
-        string='Partner of Accounting Move',
+        related="move_id.partner_id",
+        string="Partner of Accounting Move",
         readonly=True,
         store=True,
-        help=('The partner that is connected to the accounting move related '
-              'to this analytic line')
+        help=(
+            "The partner that is connected to the accounting move related "
+            "to this analytic line"
+        ),
     )
