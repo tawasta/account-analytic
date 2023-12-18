92e6606090b69232e8c4c2fa8117e9cfa531461e
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-12-18 07:36:29 +0000

    [IMP] account_invoice_used_analytic_tags module: Compute when a line tag has...

--- account_invoice_used_analytic_tags/__manifest__.py
+++ account_invoice_used_analytic_tags/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Lines' analytic tags for invoices",
     "summary": "Enables filtering invoices based on lines' analytic tags",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Invoicing",
     "website": "https://gitlab.com/tawasta/odoo/account-analytic",
     "author": "Tawasta",
