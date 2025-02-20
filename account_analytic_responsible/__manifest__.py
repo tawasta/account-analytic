8016600e82a27f93285be164998437cd567b6a1a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2025-02-20 15:59:31 +0200

    [ADD] Github CI, update dotfiles

--- account_analytic_responsible/__manifest__.py
+++ account_analytic_responsible/__manifest__.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
-#    Author: Oy Tawasta OS Technologies Ltd.
-#    Copyright 2024 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#    Author: Futural Oy
+#    Copyright 2024 Futural Oy (https://futural)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -23,8 +23,8 @@
     "summary": "Assign a responsible user for an analytic account",
     "version": "17.0.1.0.1",
     "category": "Accounting",
-    "website": "https://gitlab.com/tawasta/odoo/account-analytic",
-    "author": "Tawasta",
+    "website": "https://github.com/tawasta/account-analytic",
+    "author": "Futural",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
--- account_analytic_responsible/models/analytic_account.py
+++ account_analytic_responsible/models/analytic_account.py
@@ -2,7 +2,6 @@ from odoo import fields, models
 
 
 class AccountAnalyticAccount(models.Model):
-
     _inherit = "account.analytic.account"
 
     responsible_id = fields.Many2one("res.users", string="Responsible", copy=False)
